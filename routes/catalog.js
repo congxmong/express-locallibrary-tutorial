c29c4a4d279cf6788fdf68285de5e92c8be868e9
Cong Pham <cong.pham@aliznet.fr>
2019-01-17 13:54:33 +0100

    change folder uploads

--- routes/catalog.js
+++ routes/catalog.js
@@ -10,7 +10,7 @@ var book_instance_controller = require('../controllers/bookinstanceController');
 var multer = require('multer');
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './public/images');
+        cb(null, './uploads');
     },
     filename: function (req, file, cb) {
         // cb(null, file.originalname + '-' + Date.now());
